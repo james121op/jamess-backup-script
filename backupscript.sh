cb8749225ee4d3ffb680614683b95f346ddfb2bf
James <james121op@gmail.com>
2022-05-30 22:13:35 +0100

    FIRST COMMIT

---
+++ backupscript.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#prerequirements
+apt install zip
+
+BackupLocation="/backups"
+
+PterodactylUserData="/var/lib/pterodactyl"
+NginxConfigs="/etc/nginx"
+WebsiteContent="/var/www"
+SqlUser="root"
+SqlPass=""
+
+RcloneRemote=""
+RcloneLocation=""
+
+SftpServer="1.1.1.1"
+SftpUser="user"
+SftpPassword="password"
+
+CurrentDate=$(date | sed 's/\ /-/g')
+mkdir $BackupLocation/$CurrentDate
+cp -r {$PterodactylUserData, $NginxConfigs, $WebsiteContent} $BackupLocation/$CurrentDate
+cp $PterodactylUserData $BackupLocation/$CurrentDate
+
+#export mariadb database(s)
+mysqldump -u "$SqlUser" -p "$SqlPass" --all-databases > $BackupLocation/$CurrentDate/databases.sql
+
+#compress
+zip -r9 $CurrentDate.tar.gz $CurrentDate
