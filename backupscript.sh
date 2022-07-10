2bf3f8f1940d7769a4b78d5c81226bda69560f30
James <james121op@gmail.com>
2022-07-11 00:23:11 +0100

    Update backupscript.sh

--- backupscript.sh
+++ backupscript.sh
@@ -18,17 +18,17 @@ SqlPass=""
 
 #mount netwrok drive, for automatic auth look in /etc/davfs2/secrets
 mkdir /backups
-mount -t davfs $MountUrl $MountPoint
+mount --types davfs $MountUrl $MountPoint
 
 CurrentDate=$(date | sed 's/\ /_/g' | sed 's/\:/_/g')
-mkdir -p $TempLocation/$CurrentDate
-cp -vr {$PterodactylUserData,$NginxConfigs,$WebsiteContent} $TempLocation/$CurrentDate
+mkdir --verbose --parents $TempLocation/$CurrentDate
+cp --verbose --recursive {$PterodactylUserData,$NginxConfigs,$WebsiteContent} $TempLocation/$CurrentDate
 
 #export mariadb database(s)
 mysqldump --user="$SqlUser" --password="$SqlPass" --all-databases > $TempLocation/$CurrentDate/databases.sql
 
 #compress
-zip -cJvf $TempLocation/$CurrentDate.tar.xz $TempLocation/$CurrentDate
+tar --create --xz --verbose --file=$TempLocation/$CurrentDate.tar.xz $TempLocation/$CurrentDate
 #move zip to network drive
 cp $TempLocation/$CurrentDate.tar.xz $MountPoint
 #unmount Mount Point
