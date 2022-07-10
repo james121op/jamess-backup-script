d61bd301a740873a66bba4238d934ad4e23d5bec
James <james121op@gmail.com>
2022-07-10 18:14:29 +0100

    untested

--- backupscript.sh
+++ backupscript.sh
@@ -2,23 +2,34 @@
 
 #prerequirements
 apt install zip -y
+apt install davfs2 -y
 
-BackupLocation="/backups"
+
+MountPoint="/backups"
+MountUrl="example.com/backups"
+
+TempLocation="/tmp"
 
 PterodactylUserData="/var/lib/pterodactyl"
 NginxConfigs="/etc/nginx"
 WebsiteContent="/var/www"
 SqlUser="root"
-SqlPass="."
+SqlPass=""
+
+#mount netwrok drive, for automatic auth look in /etc/davfs2/secrets
+mkdir /backups
+mount -t davfs $MountUrl $MountPoint
 
 CurrentDate=$(date | sed 's/\ /_/g')
-mkdir -p $BackupLocation/$CurrentDate
-cp -vr {$PterodactylUserData,$NginxConfigs,$WebsiteContent} $BackupLocation/$CurrentDate
+mkdir -p $TempLocation/$CurrentDate
+cp -vr {$PterodactylUserData,$NginxConfigs,$WebsiteContent} $TempLocation/$CurrentDate
 
 #export mariadb database(s)
-mysqldump --user="$SqlUser" --password="$SqlPass" --all-databases > $BackupLocation/$CurrentDate/databases.sql
+mysqldump --user="$SqlUser" --password="$SqlPass" --all-databases > $TempLocation/$CurrentDate/databases.sql
 
 #compress
-zip -r9 $BackupLocation/$CurrentDate.zip $BackupLocation/$CurrentDate
-#remove uncompressed version
-rm -rf $BackupLocation/$CurrentDate
+zip -r9 $TempLocation/$CurrentDate.zip $TempLocation/$CurrentDate
+#move zip to network drive
+cp $TempLocation/$CurrentDate.zip $MountPoint
+#remove Temp Location
+rm -rf $TempLocation
